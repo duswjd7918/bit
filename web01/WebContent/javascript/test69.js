957ee24f7af0d627894b1a136c14ccc739db79ae
patience89 <patience_89@naver.com>
2014-10-06 17:19:53 +0900

    .

--- web01/WebContent/javascript/test69.js
+++ web01/WebContent/javascript/test69.js
@@ -1,11 +1,13 @@
 "use strict"; 
 
-//목표: 라이브러리화2? 
-//기존 엘리먼트 객체에 나만의 조리료를 뿌리자!
+//목표: 라이브러리화5 
+//val() 함수에 읽기 기능 추가
+//삭제, 변경 버튼의 리스너 추가
+//bit() 함수를 변경 => querySelectorAll()을 사용하여 처리.
+
+
 
 changeState('create');
-
-var toYYYYMMDD = new Date();
 
 function changeState(state) {
 	var stateMap = {
@@ -15,16 +17,8 @@ function changeState(state) {
 
 	stateMap[state] = '';
 
-	var detailClass = document.querySelectorAll('.detail');
-	var createClass = document.querySelectorAll('.create');
-
-	for (var i = 0; i < detailClass.length; i++) {
-		detailClass[i].style.display = stateMap.detail;
-	}
-
-	for (var i = 0; i < createClass.length; i++) {
-		createClass[i].style.display = stateMap.create;
-	}
+	$('.detail').css('display', stateMap.detail);
+	$('.create').css('display', stateMap.create);
 }
 
 //게시글을 저장하는 객체 생성자 함수
@@ -38,79 +32,64 @@ function Board(title, content, writer, password) {
 }
 
 function resetForm() {
-	// reset 버튼에게 click 이벤트를 전달 
-	//1) MouseEvent 객체 생성
-	var event = new MouseEvent('click', {
-		'view': window,
-		'bubbles': true,
-		'cancelable': true
-	});
-
-	//2) reset  버튼에게 이벤트 전달
-	$('#btnCancel').dispatchEvent(event);
+	$('#btnCancel').click();
 }
 
 var boardList = [];
 
-$('#btnCancel').onclick = function(event) {
+$('#btnCancel').click(function(event) {
 	changeState('create');
-}
+});
 
-$('#btnAdd').onclick = function(event) {
+$('#btnAdd').click(function(event) {
 	var board = new Board(
-			$('#title').value,
-			$('#content').value,
-			$('#writer').value,
-			$('#password').value);
+			$('#title').val(),
+			$('#content').val(),
+			$('#writer').val(),
+			$('#password').val());
 
 	boardList.push(board);
 
 	resetForm();
 
 	refreshBoardList();
-};
+});
+
+$('#btnDelete').click(function(event){
+	var no = $('#no').val();
+	boardList.splice(no,1);
+	refreshBoardList();
+	resetForm();
+});
+
+$('#btnChange').click(function(event){
+	var no = $('#no').val();
+	var board = boardList[no];
+	board.title = $('#title').val();
+	board.content = $('#content').val();
+	refreshBoardList();
+});
 
 function refreshBoardList() {
 	var boardTable = $('#boardTable');
-	//var tbody = boardTable.children[0]; // <tbody>
-	var tbody = boardTable.firstElementChild; // <tbody>
-	for (var i = tbody.children.length -1 ; i > 0; i--) {
-		//console.log(tbody.children[i]);
-		tbody.removeChild(tbody.children[i]);
-	}
+	
+	$('.dataRow').remove();
 
-	var tr = null;
-	var td = null;
-	var a = null;
 	for (var i in boardList) {
-		tr = $('<tr>');
-
-		td = $('<td>');
-		td.html(i);
-		tr.appendChild(td);
-
-		td = $('<td>');   
-		a = $('<a>');
-		a.setAttribute('bno', new String(i));
-		a.href = '#';
-		a.onclick = loadBoardDetail;
-		a.html(boardList[i].title); 
-		td.appendChild(a);
-		tr.appendChild(td);
-
-		td = $('<td>');
-		td.html(boardList[i].writer);
-		tr.appendChild(td);
-
-		td = $('<td>');
-		td.html($.toYYYYMMDD(boardList[i].date));
-		tr.appendChild(td);
-
-		td = $('<td>');
-		td.html(boardList[i].count);
-		tr.appendChild(td);
-
-		tbody.appendChild(tr);
+		$('<tr>')
+			.appendTo(boardTable)
+			.attr('class', 'dataRow')
+			.append($('<td>').html(i))
+			.append($('<td>')
+					.append($('<a>')
+							.attr('bno', new String(i))
+							.attr('href', '#')
+							.click(loadBoardDetail)
+							.html(boardList[i].title)))
+			.append($('<td>').html(boardList[i].writer))
+			.append($('<td>').html($.toYYYYMMDD(boardList[i].date)))
+			.append($('<td>').html(boardList[i].count));
+			
 	}
 }
 
@@ -119,12 +98,12 @@ function loadBoardDetail(event) {
 
 	changeState('detail');
 
-	var board = boardList[this.getAttribute('bno')];
-	$('#no').value = this.getAttribute('bno');
-	$('#title').value = board.title;
-	$('#content').value = board.content;
-	$('#writer').value = board.writer;
-	$('#date').value = $.toYYYYMMDD(board.date);
+	var board = boardList[$(this).attr('bno')];
+	$('#no').val($(this).attr('bno'));
+	$('#title').val(board.title);
+	$('#content').val(board.content);
+	$('#writer').val(board.writer);
+	$('#date').val($.toYYYYMMDD(board.date));
 }
 
 
--- web01/WebContent/javascript/test69_common.js
+++ web01/WebContent/javascript/test69_common.js
@@ -2,31 +2,121 @@
 
 //window.bit = function(value) {
 function bit(value) {
-	var element = null;
-	if (value.charAt(0) == '#') { // 아이디일 경우,
-	  element = document.getElementById(value.substring(1));
+	var elements = null;
+	
+	if (value instanceof Element) {
+		elements = [value];
 	} else if (value.charAt(0) == '<') { // 태그일 경우,
-	  element = document.createElement(value.replace(/<|>/g, ''));	
+		elements = [document.createElement(value.replace(/<|>/g, ''))];	
+	} else {
+		elements = document.querySelectorAll(value);
 	}
-	
-	element.text = function(value) {
-		this.textContent = value;
+
+	elements.text = function(value) {
+		for (var i = 0; i < this.length; i++) {
+			this[i].textContent = value;
+		}
+		return this;
+	};
+
+	elements.html = function(value) {
+		for (var i = 0; i < this.length; i++) {
+			this[i].innerHTML = value;
+		}
+		return this;createClass[i]
+	};
+
+	elements.append = function(child) {
+		for (var i = 0; i < this.length; i++) {
+			if (child instanceof Element) {
+				this[i].appendChild(child);
+			} else {
+				for (var x = 0; x < child.length; x++) {
+					this[i].appendChild(child[x]);
+				}
+			}
+		}
+		return this;
+	};
+
+	elements.appendTo = function(parent) {
+		for (var i = 0; i < this.length; i++) {
+			if (parent instanceof Element) {
+				parent.appendChild(this[i]);
+			} else {
+				parent[0].appendChild(this[i]);
+			}
+		}
+		return this;
+	};
+
+	elements.attr = function(name, value) {
+		if (arguments.length == 2) {
+			for (var i = 0; i < this.length; i++) {
+				this[i].setAttribute(name, value);
+			}
+			return this;
+		} else {
+			return this[0].getAttribute(name);
+		}
+	};
+
+	elements.click = function(listener) {
+		for (var i = 0; i < this.length; i++) {
+			if (listener) {
+				this[i].onclick = listener;
+			} else {
+				var event = new MouseEvent('click', {
+					'view': window,
+					'bubbles': true,
+					'cancelable': true
+				});
+				this[i].dispatchEvent(event);
+			}
+		}
+		return this;
+	}
+
+	elements.val = function(value) {
+		if (arguments.length == 1) {
+			for (var i = 0; i < this.length; i++) {
+				this[i].value = value;
+			}
+			return this;
+		} else {
+			return this[0].value;
+		}
+	};
+
+	elements.css = function(name, value) {
+		if (arguments.length == 2) {
+			for (var i = 0; i < this.length; i++) {
+				this[i].style[name] = value;
+			}
+			return this;
+		} else {
+			return this[0].style[name];
+		}
 	};
 	
-	element.html = function(value) {
-		this.innerHTML = value;
+	elements.remove = function() {
+		for (var i = 0; i < this.length; i++) {
+			this[i].parentElement.removeChild(this[i]);
+		}
+		return this;
 	};
 	
-	return element;
+
+	return elements;
 }
 
 var $ = bit;
 
-// 함수도 객체다! 저장소로 사용될 수 있다.
+//함수도 객체다! 저장소로 사용될 수 있다.
 bit.toYYYYMMDD = function(date) {
 	return date.getFullYear() + '-' +
-	  (date.getMonth() + 1) + '-' +
-	  date.getDate();
+	(date.getMonth() + 1) + '-' +
+	date.getDate();
 };
 
 
